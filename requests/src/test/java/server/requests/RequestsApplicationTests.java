3cb7b133475c7be1a408eda0789e6fb40ed5448d
szweda <jakubszweda1@gmail.com>
2023-05-11 13:25:23 +0200

    additional files

---
+++ requests/src/test/java/server/requests/RequestsApplicationTests.java
@@ -0,0 +1,13 @@
+package server.requests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
