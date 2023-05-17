07f01764f0fae52ef417e67ac03e43c51664aeaa
mmlodnic <01178297@pw.edu.pl>
2023-05-18 01:57:19 +0200

    tests for Karol algorithm

---
+++ testKarol.java
@@ -0,0 +1,336 @@
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Tests {
+    public int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
+
+
+    @Test
+    public void testWorstExecutionTime() throws NoSuchAlgorithmException {
+        TransactionGraph graph;
+        ArrayList<User> users = new ArrayList<>();
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        users = new ArrayList<>();
+        transactions = new ArrayList<>();
+        graph = new TransactionGraph(transactions, users);
+
+        int numUsers = 10000;
+        for (int i = 0; i < numUsers; i++) {
+            users.add(new User("User", 10,"password"));
+        }
+        Random rand = new Random();
+        User sender = users.get(rand.nextInt(numUsers));
+        User receiver = users.get(rand.nextInt(numUsers));
+        while (sender == receiver) {
+            receiver = users.get(rand.nextInt(numUsers));
+        }
+        int numTransactions = 100000;
+        for (int i = 0; i < numTransactions; i++) {
+            transactions.add(new Transaction(i, sender, receiver, rand.nextInt(), Date.from(LocalDate.now().atStartOfDay(ZoneId.of("Europe/Warsaw")).toInstant())));
+        }
+        long startTime = System.currentTimeMillis();
+        graph.simplify();
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        assertTrue(duration < 1000);
+    }
+
+    @Test
+    public void testAverageCaseExecutionTime() throws NoSuchAlgorithmException {
+        TransactionGraph graph;
+        ArrayList<User> users = new ArrayList<>();
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        users = new ArrayList<>();
+        transactions = new ArrayList<>();
+        graph = new TransactionGraph(transactions, users);
+
+        int numUsers = 1000;
+        for (int i = 0; i < numUsers; i++) {
+            users.add(new User("User", 10,"password"));
+        }
+        Random rand = new Random();
+        int numTransactions = 10000;
+        for (int i = 0; i < numTransactions; i++) {
+            User sender = users.get(rand.nextInt(numUsers));
+            User receiver = users.get(rand.nextInt(numUsers));
+            while (sender == receiver) {
+                receiver = users.get(rand.nextInt(numUsers));
+            }
+            transactions.add(new Transaction(i, sender, receiver, rand.nextInt(), Date.from(LocalDate.now().atStartOfDay(ZoneId.of("Europe/Warsaw")).toInstant())));
+        }
+        long startTime = System.currentTimeMillis();
+
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        assertTrue(duration < 100);
+    }
+
+
+    @Test
+    public void test5() throws NoSuchAlgorithmException {
+
+        ArrayList<User> users = new ArrayList<>();
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        long startTime = System.currentTimeMillis();
+        users = new ArrayList<>();
+        transactions = new ArrayList<>();
+
+        User user1 = new User("piotr1", 1, "passwd1");
+        User user2 = new User("piotr2", 2, "passwd2");
+        User user3 = new User("piotr3", 3, "passwd3");
+        User user4 = new User("piotr4", 4, "passwd4");
+        User user5 = new User("piotr5", 5, "passwd5");
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+        users.add(user4);
+        users.add(user5);
+
+        TransactionGraph graph = new TransactionGraph(transactions, users);
+
+        graph.addTransaction(new Transaction(1, user2, user1, 20, Date.from(LocalDate.now().atStartOfDay(ZoneId.of("Europe/Warsaw")).toInstant())));
+        graph.addTransaction(new Transaction(2, user5, user2, 40, Date.from(LocalDate.now().atStartOfDay(ZoneId.of("Europe/Warsaw")).toInstant())));
+        graph.addTransaction(new Transaction(3, user5, user4, 10, Date.from(LocalDate.now().atStartOfDay(ZoneId.of("Europe/Warsaw")).toInstant())));
+        graph.addTransaction(new Transaction(4, user3, user4, 20, Date.from(LocalDate.now().atStartOfDay(ZoneId.of("Europe/Warsaw")).toInstant())));
+        graph.addTransaction(new Transaction(5, user1, user4, 10, Date.from(LocalDate.now().atStartOfDay(ZoneId.of("Europe/Warsaw")).toInstant())));
+
+        graph.simplify();
+
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println(duration);
+    }
+
+    @Test
+    public void test1000() throws NoSuchAlgorithmException {
+
+        ArrayList<User> users = new ArrayList<>();
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        long startTime = System.currentTimeMillis();
+        users = new ArrayList<>();
+        transactions = new ArrayList<>();
+
+        for(int i = 0; i < 1000; i++) {
+            users.add(new User(Integer.toString(i), i, "passwd"));
+        }
+
+        TransactionGraph graph = new TransactionGraph(transactions, users);
+
+        for(int i = 0; i < 1000; i++) {
+            User sender = users.get(getRandomNumber(1,999));
+            User receiver = users.get(getRandomNumber(1,999));
+            graph.addTransaction(new Transaction(i, sender,
+                    receiver,getRandomNumber(0,10),Date.from(LocalDate.now().atStartOfDay(ZoneId.of("Europe/Warsaw")).toInstant())));
+        }
+
+        /*
+        List<Transaction> list = graph.simplify(graph.users, graph.listOfTransactions);
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).getReceiver().getUsername() + " -> " +
+                    list.get(i).getSender().getUsername() + " " + list.get(i).getAmount());
+        }
+        */
+        graph.simplify();
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println(duration);
+    }
+
+    @Test
+    public void test10000() throws NoSuchAlgorithmException {
+
+        ArrayList<User> users = new ArrayList<>();
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        long startTime = System.currentTimeMillis();
+        users = new ArrayList<>();
+        transactions = new ArrayList<>();
+
+        for(int i = 0; i < 10000; i++) {
+            users.add(new User(Integer.toString(i), i, "passwd"));
+        }
+
+        TransactionGraph graph = new TransactionGraph(transactions, users);
+
+        for(int i = 0; i < 10000; i++) {
+            User sender = users.get(getRandomNumber(1,9999));
+            User receiver = users.get(getRandomNumber(1,9999));
+            graph.addTransaction(new Transaction(i, sender,
+                    receiver,getRandomNumber(0,10),Date.from(LocalDate.now().atStartOfDay(ZoneId.of("Europe/Warsaw")).toInstant())));
+        }
+
+        /*
+        List<Transaction> list = graph.simplify(graph.users, graph.listOfTransactions);
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).getReceiver().getUsername() + " -> " +
+                    list.get(i).getSender().getUsername() + " " + list.get(i).getAmount());
+        }
+        */
+        graph.simplify();
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println(duration);
+    }
+
+    @Test
+    public void test20000() throws NoSuchAlgorithmException {
+
+        ArrayList<User> users = new ArrayList<>();
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        long startTime = System.currentTimeMillis();
+        users = new ArrayList<>();
+        transactions = new ArrayList<>();
+
+        for(int i = 0; i < 20000; i++) {
+            users.add(new User(Integer.toString(i), i, "passwd"));
+        }
+
+        TransactionGraph graph = new TransactionGraph(transactions, users);
+
+        for(int i = 0; i < 20000; i++) {
+            User sender = users.get(getRandomNumber(1,19999));
+            User receiver = users.get(getRandomNumber(1,19999));
+            graph.addTransaction(new Transaction(i, sender,
+                    receiver,getRandomNumber(0,10),Date.from(LocalDate.now().atStartOfDay(ZoneId.of("Europe/Warsaw")).toInstant())));
+        }
+
+        /*
+        List<Transaction> list = graph.simplify(graph.users, graph.listOfTransactions);
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).getReceiver().getUsername() + " -> " +
+                    list.get(i).getSender().getUsername() + " " + list.get(i).getAmount());
+        }
+        */
+        graph.simplify();
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println(duration);
+    }
+
+    @Test
+    public void test20000Value10000() throws NoSuchAlgorithmException {
+
+        ArrayList<User> users = new ArrayList<>();
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        long startTime = System.currentTimeMillis();
+        users = new ArrayList<>();
+        transactions = new ArrayList<>();
+
+        for(int i = 0; i < 20000; i++) {
+            users.add(new User(Integer.toString(i), i, "passwd"));
+        }
+
+        TransactionGraph graph = new TransactionGraph(transactions, users);
+
+        for(int i = 0; i < 20000; i++) {
+            User sender = users.get(getRandomNumber(1,19999));
+            User receiver = users.get(getRandomNumber(1,19999));
+            graph.addTransaction(new Transaction(i, sender,
+                    receiver,getRandomNumber(0,10000),Date.from(LocalDate.now().atStartOfDay(ZoneId.of("Europe/Warsaw")).toInstant())));
+        }
+
+        /*
+        List<Transaction> list = graph.simplify(graph.users, graph.listOfTransactions);
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).getReceiver().getUsername() + " -> " +
+                    list.get(i).getSender().getUsername() + " " + list.get(i).getAmount());
+        }
+        */
+        graph.simplify();
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println(duration);
+    }
+
+    @Test
+    public void test20000Value10000000() throws NoSuchAlgorithmException {
+
+        ArrayList<User> users = new ArrayList<>();
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        long startTime = System.currentTimeMillis();
+        users = new ArrayList<>();
+        transactions = new ArrayList<>();
+
+        for(int i = 0; i < 20000; i++) {
+            users.add(new User(Integer.toString(i), i, "passwd"));
+        }
+
+        TransactionGraph graph = new TransactionGraph(transactions, users);
+
+        for(int i = 0; i < 20000; i++) {
+            User sender = users.get(getRandomNumber(1,19999));
+            User receiver = users.get(getRandomNumber(1,19999));
+            graph.addTransaction(new Transaction(i, sender,
+                    receiver,getRandomNumber(0,10000000),Date.from(LocalDate.now().atStartOfDay(ZoneId.of("Europe/Warsaw")).toInstant())));
+        }
+
+        /*
+        List<Transaction> list = graph.simplify(graph.users, graph.listOfTransactions);
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).getReceiver().getUsername() + " -> " +
+                    list.get(i).getSender().getUsername() + " " + list.get(i).getAmount());
+        }
+        */
+        graph.simplify();
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println(duration);
+    }
+
+    @Test
+    public void test25000Value10000000() throws NoSuchAlgorithmException {
+
+        ArrayList<User> users = new ArrayList<>();
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        long startTime = System.currentTimeMillis();
+        users = new ArrayList<>();
+        transactions = new ArrayList<>();
+
+        for(int i = 0; i < 25000; i++) {
+            users.add(new User(Integer.toString(i), i, "passwd"));
+        }
+
+        TransactionGraph graph = new TransactionGraph(transactions, users);
+
+        for(int i = 0; i < 25000; i++) {
+            User sender = users.get(getRandomNumber(1,24999));
+            User receiver = users.get(getRandomNumber(1,24999));
+            graph.addTransaction(new Transaction(i, sender,
+                    receiver,getRandomNumber(9000000,10000000),Date.from(LocalDate.now().atStartOfDay(ZoneId.of("Europe/Warsaw")).toInstant())));
+        }
+
+        /*
+        List<Transaction> list = graph.simplify(graph.users, graph.listOfTransactions);
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).getReceiver().getUsername() + " -> " +
+                    list.get(i).getSender().getUsername() + " " + list.get(i).getAmount());
+        }
+        */
+        graph.simplify();
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println(duration);
+    }
+
+
+}
+
