77deef545c8aef42ec890d694f1e979bafd92630
Jakub T <jakubtrzcinka@gmail.com>
2023-06-12 15:43:24 +0200

    Changed server address

--- IoucashAndroid/app/src/main/java/com/aproteam/ioucash/Constants.java
+++ IoucashAndroid/app/src/main/java/com/aproteam/ioucash/Constants.java
@@ -2,6 +2,6 @@ package com.aproteam.ioucash;
 
 public class Constants {
 
-	public static final String API_BASE_URL = "http://www.ioucash.online";
+	public static final String API_BASE_URL = "http://57.128.195.110:8080";
 
 }
